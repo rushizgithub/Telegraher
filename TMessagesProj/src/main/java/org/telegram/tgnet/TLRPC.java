ec57a6a290caee4fd6a90e82b69096eebfd6e154
Nikita S <nikita@saraeff.net>
2022-02-02 15:43:27 +0100

    disable `edit_hide`
    (cherry picked from commit 658b839743668cf3b7e05ed5abe3d5ac64108e52)
    (cherry picked from commit 04dd795caa3b84af7d20df43b6c0da8ef1d8141a)
    (cherry picked from commit b34cf7889865c7566dcb8ef191a39df0844bc405)
    (cherry picked from commit 700f355bb0e495c0dd3ae806f9b187d2d20dfd64)
    (cherry picked from commit 82d3ff7aedd99428ad06c71c2a951322cce57ca8)

--- TMessagesProj/src/main/java/org/telegram/tgnet/TLRPC.java
+++ TMessagesProj/src/main/java/org/telegram/tgnet/TLRPC.java
@@ -58175,7 +58175,7 @@ public class TLRPC {
             post = (flags & 16384) != 0;
             from_scheduled = (flags & 262144) != 0;
             legacy = (flags & 524288) != 0;
-            edit_hide = (flags & 2097152) != 0;
+            edit_hide = false;
             pinned = (flags & 16777216) != 0;
             noforwards = (flags & 67108864) != 0;
             id = stream.readInt32(exception);
@@ -58358,7 +58358,7 @@ public class TLRPC {
             post = (flags & 16384) != 0;
             from_scheduled = (flags & 262144) != 0;
             legacy = (flags & 524288) != 0;
-            edit_hide = (flags & 2097152) != 0;
+            edit_hide = false;
             pinned = (flags & 16777216) != 0;
             noforwards = (flags & 67108864) != 0;
             id = stream.readInt32(exception);
@@ -58535,7 +58535,7 @@ public class TLRPC {
             post = (flags & 16384) != 0;
             from_scheduled = (flags & 262144) != 0;
             legacy = (flags & 524288) != 0;
-            edit_hide = (flags & 2097152) != 0;
+            edit_hide = false;
             pinned = (flags & 16777216) != 0;
             id = stream.readInt32(exception);
             if ((flags & 256) != 0) {
@@ -58710,7 +58710,7 @@ public class TLRPC {
             post = (flags & 16384) != 0;
             from_scheduled = (flags & 262144) != 0;
             legacy = (flags & 524288) != 0;
-            edit_hide = (flags & 2097152) != 0;
+            edit_hide = false;
             pinned = (flags & 16777216) != 0;
             id = stream.readInt32(exception);
             if ((flags & 256) != 0) {
@@ -58879,7 +58879,7 @@ public class TLRPC {
             post = (flags & 16384) != 0;
             from_scheduled = (flags & 262144) != 0;
             legacy = (flags & 524288) != 0;
-            edit_hide = (flags & 2097152) != 0;
+            edit_hide = false;
             id = stream.readInt32(exception);
             if ((flags & 256) != 0) {
                 from_id = new TLRPC.TL_peerUser();
@@ -59042,7 +59042,7 @@ public class TLRPC {
             post = (flags & 16384) != 0;
             from_scheduled = (flags & 262144) != 0;
             legacy = (flags & 524288) != 0;
-            edit_hide = (flags & 2097152) != 0;
+            edit_hide = false;
             id = stream.readInt32(exception);
             if ((flags & 256) != 0) {
                 from_id = new TLRPC.TL_peerUser();
@@ -59199,7 +59199,7 @@ public class TLRPC {
             post = (flags & 16384) != 0;
             from_scheduled = (flags & 262144) != 0;
             legacy = (flags & 524288) != 0;
-            edit_hide = (flags & 2097152) != 0;
+            edit_hide = false;
             id = stream.readInt32(exception);
             if ((flags & 256) != 0) {
                 from_id = new TLRPC.TL_peerUser();
@@ -59362,7 +59362,7 @@ public class TLRPC {
             post = (flags & 16384) != 0;
             from_scheduled = (flags & 262144) != 0;
             legacy = (flags & 524288) != 0;
-            edit_hide = (flags & 2097152) != 0;
+            edit_hide = false;
             id = stream.readInt32(exception);
             if ((flags & 256) != 0) {
                 from_id = new TLRPC.TL_peerUser();
