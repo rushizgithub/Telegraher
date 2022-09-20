334982d7494ecf4e64eac59f710d56a0e18a3b01
Nikita S <nikita@saraeff.net>
2022-09-20 23:11:16 +0200

    use buildvars in contacts also

--- TMessagesProj/src/main/java/org/telegram/messenger/ContactsController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/ContactsController.java
@@ -324,7 +324,7 @@ public class ContactsController extends BaseController {
         if (true) return;
         AccountManager am = AccountManager.get(ApplicationLoader.applicationContext);
         try {
-            Account[] accounts = am.getAccountsByType("org.telegram.messenger");
+            Account[] accounts = am.getAccountsByType(BuildVars.BUILD_DUROV);
             systemAccount = null;
             for (int a = 0; a < accounts.length; a++) {
                 Account acc = accounts[a];
@@ -357,7 +357,7 @@ public class ContactsController extends BaseController {
             readContacts();
             if (systemAccount == null) {
                 try {
-                    systemAccount = new Account("" + getUserConfig().getClientUserId(), "org.telegram.messenger");
+                    systemAccount = new Account("" + getUserConfig().getClientUserId(), BuildVars.BUILD_DUROV);
                     am.addAccountExplicitly(systemAccount, "", null);
                 } catch (Exception ignore) {
 
@@ -371,7 +371,7 @@ public class ContactsController extends BaseController {
         try {
             systemAccount = null;
             AccountManager am = AccountManager.get(ApplicationLoader.applicationContext);
-            Account[] accounts = am.getAccountsByType("org.telegram.messenger");
+            Account[] accounts = am.getAccountsByType(BuildVars.BUILD_DUROV);
             for (int a = 0; a < accounts.length; a++) {
                 Account acc = accounts[a];
                 boolean found = false;
@@ -448,7 +448,7 @@ public class ContactsController extends BaseController {
                     if (false) {
                         AccountManager am = AccountManager.get(ApplicationLoader.applicationContext);
                         try {
-                            Account[] accounts = am.getAccountsByType("org.telegram.messenger");
+                            Account[] accounts = am.getAccountsByType(BuildVars.BUILD_DUROV);
                             systemAccount = null;
                             for (int a = 0; a < accounts.length; a++) {
                                 Account acc = accounts[a];
@@ -466,7 +466,7 @@ public class ContactsController extends BaseController {
 
                         }
                         try {
-                            systemAccount = new Account("" + getUserConfig().getClientUserId(), "org.telegram.messenger");
+                            systemAccount = new Account("" + getUserConfig().getClientUserId(), BuildVars.BUILD_DUROV);
                             am.addAccountExplicitly(systemAccount, "", null);
                         } catch (Exception ignore) {
 
@@ -2051,7 +2051,7 @@ public class ContactsController extends BaseController {
         ContentResolver contentResolver = ApplicationLoader.applicationContext.getContentResolver();
         if (check) {
             try {
-                Uri rawContactUri = ContactsContract.RawContacts.CONTENT_URI.buildUpon().appendQueryParameter(ContactsContract.CALLER_IS_SYNCADAPTER, "true").appendQueryParameter(ContactsContract.RawContacts.ACCOUNT_NAME, Long.valueOf(UserConfig.getInstance(currentAccount).getClientUserId()).toString()).appendQueryParameter(ContactsContract.RawContacts.ACCOUNT_TYPE, "org.telegram.messenger").build();
+                Uri rawContactUri = ContactsContract.RawContacts.CONTENT_URI.buildUpon().appendQueryParameter(ContactsContract.CALLER_IS_SYNCADAPTER, "true").appendQueryParameter(ContactsContract.RawContacts.ACCOUNT_NAME, Long.valueOf(UserConfig.getInstance(currentAccount).getClientUserId()).toString()).appendQueryParameter(ContactsContract.RawContacts.ACCOUNT_TYPE, BuildVars.BUILD_DUROV).build();
                 int value = contentResolver.delete(rawContactUri, ContactsContract.RawContacts.SYNC2 + " = " + user.id, null);
             } catch (Exception ignore) {
 
